043fc01562a65c374d1838028aa168ff2ecda721
harshaboi <sriharshaplayschess@gmail.com>
2025-02-27 20:26:33 -0500

    hopefully this commits

---
+++ Assets/Scripts/Player Movement.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private int hello = 0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
