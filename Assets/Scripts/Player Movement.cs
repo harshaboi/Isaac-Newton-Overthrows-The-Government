7795e1b88b377adc2ba98cbdec46e86aa1e58270
harshaboi <sriharshaplayschess@gmail.com>
2025-03-03 11:02:48 -0500

    player movement progress v1 (player camera working)

--- Assets/Scripts/Player Movement.cs
+++ Assets/Scripts/Player Movement.cs
@@ -1,26 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private int hello = 0;
-    private int x = 2;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+    public float speed = 5f;
+    [SerializeField] private Camera playerCamera;
+    private float horizAxis;
+    private float vertAxis;
+    private float horizRotate;
+    private float vertRotate;
+    private void Start(){
+        playerCamera.transform.position = transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        hello++;
-        if(hello == 100){
-            hello = 0;
-        }else if(hello == 50){
-            hello = 51;
-        }else if(x < 1){
-            x++;
-        }
+        playerCamera.transform.position = transform.position;
+        horizAxis = Input.GetAxis("Horizontal");
+        vertAxis = Input.GetAxis("Vertical");
+        horizRotate = Input.GetAxis("Mouse X");
+        vertRotate = Input.GetAxis("Mouse Y");
 
+        //transform.eulerAngles += speed * new Vector3(vertRotate, 0, horizRotate);
+        playerCamera.transform.eulerAngles += speed * new Vector3(vertRotate, horizRotate, 0);
+    }
+    private void FixedUpdate()
+    {
+        
     }
 }
