c850097fa0fd8a6023863e8a31ccbf2226606ed2
harshaboi <sriharshaplayschess@gmail.com>
2025-03-03 11:21:04 -0500

    player movement kinda added with movement direction change with camera direction

--- Assets/Scripts/Player Movement.cs
+++ Assets/Scripts/Player Movement.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
+
+    public float slowFactor = 3;
+
     [SerializeField] private Camera playerCamera;
     private float horizAxis;
     private float vertAxis;
     private float horizRotate;
     private float vertRotate;
+
+    private float speedRatioX;
+    private float speedRatioZ;
     private void Start(){
         playerCamera.transform.position = transform.position;
     }
@@ -21,9 +27,15 @@ public class PlayerMovement : MonoBehaviour
         vertAxis = Input.GetAxis("Vertical");
         horizRotate = Input.GetAxis("Mouse X");
         vertRotate = Input.GetAxis("Mouse Y");
+        speedRatioX = vertAxis / slowFactor;
+        speedRatioZ = horizAxis / slowFactor;
+        
+        Vector3 moveDirection = speed * vertAxis * transform.forward;
+        transform.position += moveDirection * Time.deltaTime;
 
-        //transform.eulerAngles += speed * new Vector3(vertRotate, 0, horizRotate);
+        
         playerCamera.transform.eulerAngles += speed * new Vector3(vertRotate, horizRotate, 0);
+        transform.eulerAngles += speed * new Vector3(0, horizRotate, 0);
     }
     private void FixedUpdate()
     {
