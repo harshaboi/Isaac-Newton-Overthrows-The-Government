f997ce8df6a2924faa87c36fc561caeeb0b73ffa
harshaboi <sriharshaplayschess@gmail.com>
2025-03-06 11:41:32 -0500

    Added dashing (tweak more for better feel)

--- Assets/Scripts/Player Movement.cs
+++ Assets/Scripts/Player Movement.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -11,23 +13,38 @@ public class PlayerMovement : MonoBehaviour
     private float yRot;
 
     private CharacterController controller;
+    private Rigidbody rb;
     [SerializeField] private Transform playerCam;
     [SerializeField] private float speed;
     [SerializeField] private float sensitivity;
     [SerializeField] private float jumpForce;
-    private float gravity = -9.81f;
+    public float dashSpeed;
+    private int dashCool;
+    private readonly int dashThreshold = 100;
+    private readonly float gravity = -9.81f;
     private int jumpCount = 0;
+    private float horizAxis;
+    private float vertAxis;
+
+    public int dashTimes = 3;
 
     private void Start(){
+        dashCool = dashThreshold;
+        rb = GetComponent<Rigidbody>();
         controller = GetComponent<CharacterController>();   
     }
     private void Update(){
-        playerMoveInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        horizAxis = Input.GetAxis("Horizontal");
+        vertAxis = Input.GetAxis("Vertical");
+        playerMoveInput = new Vector3(horizAxis, 0, vertAxis);
         playerMouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
         MovePlayer();
         MoveCam();
     }
 
+    private void FixedUpdate(){
+        dashCool++;
+    }
     private void MovePlayer(){
         Vector3 moveVector = transform.TransformDirection(playerMoveInput) * speed;
         controller.Move(moveVector * Time.deltaTime);
@@ -46,6 +63,19 @@ public class PlayerMovement : MonoBehaviour
             }
             velocity.y -= gravity * Time.deltaTime * -2f; 
         }
+        if((horizAxis != 0 || vertAxis != 0) && Input.GetAxis("Dash") == 1 && dashCool >= dashThreshold){
+            if(horizAxis !=0 && vertAxis != 0){
+                dash(new Vector3(horizAxis, 0, vertAxis));
+            }else if(horizAxis != 0){
+                dash(new Vector3(horizAxis, 0, 0));
+            }else{
+                dash(new Vector3(0, 0, vertAxis));
+            }
+            dashCool = 0;
+        }else if(Input.GetAxis("Dash") == 1 && dashCool >= dashThreshold){
+            dash(transform.forward);
+            dashCool = 0;
+        }
     }
 
     private void MoveCam(){
@@ -54,5 +84,11 @@ public class PlayerMovement : MonoBehaviour
         yRot -= playerMouseInput.x * sensitivity;
         transform.Rotate(0f, playerMouseInput.x * sensitivity, 0f);
         playerCam.transform.localRotation = Quaternion.Euler(xRot, -yRot, 0);
+    }
+
+    private void dash(Vector3 vector){
+        for(int i = 0; i < dashTimes; i++){
+            controller.Move(vector * dashSpeed * Time.deltaTime);
+        }
     }
 }
