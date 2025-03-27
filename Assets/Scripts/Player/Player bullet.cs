ae48d2f5929d25ae75c4b4a130410e988a25460b
harshaboi <sriharshaplayschess@gmail.com>
2025-03-27 11:29:23 -0400

    added explosion but fix explosion collisions find way to do dif types of explosions in one file

--- Assets/Scripts/Player/Player bullet.cs
+++ Assets/Scripts/Player/Player bullet.cs
@@ -4,6 +4,7 @@ using UnityEngine.TextCore.Text;
 public class NewMonoBehaviourScript : MonoBehaviour
 {
     [SerializeField] private float speed;
+    [SerializeField] private GameObject explosion;
     private GameObject player;
     private PlayerMovement p;
     private CharacterController controller;
@@ -19,6 +20,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
 
     void OnCollisionEnter(Collision collision){
         if(collision.gameObject.tag != "Player"){
+            Instantiate(explosion, transform.position, transform.rotation);
             Destroy(gameObject);
         }
     }
